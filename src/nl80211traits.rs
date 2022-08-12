330e68a218dbcefee39a7f452343115bdb80f6fa
MaxVerevkin <maxxverrr@gmail.com>
2022-08-12 14:44:21 +0300

    remove unused file

--- src/nl80211traits.rs
+++
@@ -1,7 +0,0 @@
-use crate::attr::{Attrs, Nl80211Attr};
-use neli::err::DeError;
-
-/// Parse netlink messages attributes returned by a nl80211 command
-pub trait ParseNlAttr: Sized {
-    fn parse(attrs: Attrs<'_, Nl80211Attr>) -> Result<Self, DeError>;
-}
